819f162a72be8bf6f27ff05bf07979934cad4d4a
Xinyi Zhou <xz255@cornell.edu>
2024-10-24 13:44:29 -0400

    add Rpn ConditionalExpression Test

--- a4/tests/cs2110/RpnParserTest.java
+++ a4/tests/cs2110/RpnParserTest.java
@@ -42,25 +42,25 @@ class RpnParserTest {
 
         //At least one operand is identifier and in VarTable.
         Expression expr2 = RpnParser.parse("x 1 -", Map.of());
-        assertInstanceOf(Operation.class, expr);
+        assertInstanceOf(Operation.class, expr2);
         assertEquals(1.0, expr2.eval(MapVarTable.of("x", 2.0)));
 
         //At least one operand is operation.
         Expression expr3 = RpnParser.parse("x 1 + 2 /", Map.of());
-        assertInstanceOf(Operation.class, expr);
+        assertInstanceOf(Operation.class, expr3);
         assertEquals(1.5, expr3.eval(MapVarTable.of("x", 2.0)));
 
         Expression expr4 = RpnParser.parse("x 1 + 2 3 * +", Map.of());
-        assertInstanceOf(Operation.class, expr);
+        assertInstanceOf(Operation.class, expr4);
         assertEquals(9.0, expr4.eval(MapVarTable.of("x", 2.0)));
 
         //At least one operand is Conditional.
         Expression expr5 = RpnParser.parse("x 3.0 + 2.0 y * 7.0 ?: 2 +", Map.of());
-        assertInstanceOf(Operation.class, expr);
+        assertInstanceOf(Operation.class, expr5);
         assertEquals(4.0, expr5.eval(MapVarTable.of("x", 2.0,"y",1.0)));
 
         Expression expr6 = RpnParser.parse("x 3.0 + 2.0 y * 7.0 ?: x 0.0 * 2.0 y * 7.0 ?: +", Map.of());
-        assertInstanceOf(Operation.class, expr);
+        assertInstanceOf(Operation.class, expr6);
         assertEquals(9.0, expr6.eval(MapVarTable.of("x", 2.0,"y",1.0)));
     }
 
@@ -80,10 +80,41 @@ class RpnParserTest {
             + "Conditional node evaluating to the expected value")
     void testParseConditional()
             throws UnboundVariableException, IncompleteRpnException, UndefinedFunctionException {
+
+        //branches and condition all Constant
         Expression expr = RpnParser.parse("1 2 3 ?:", UnaryFunction.mathDefs());
-        // TODO: Uncomment this test
         assertInstanceOf(Conditional.class, expr);
         assertEquals(2.0, expr.eval(MapVarTable.empty()));
+
+        Expression expr2 = RpnParser.parse("0 2 3 ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr2);
+        assertEquals(3.0, expr2.eval(MapVarTable.empty()));
+
+        //Condition not constant
+        Expression expr3 = RpnParser.parse("1 2 + 2 3 ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr3);
+        assertEquals(2.0, expr.eval(MapVarTable.empty()));
+
+        Expression expr4 = RpnParser.parse("x 2 3 ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr4);
+        assertEquals(2.0, expr4.eval(MapVarTable.of("x",1)));
+
+        Expression expr5 = RpnParser.parse("1 2 3 ?: 2 3 ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr5);
+        assertEquals(2.0, expr5.eval(MapVarTable.empty()));
+
+        //Branches not constant
+        Expression expr6 = RpnParser.parse("1 2 1 + 3 4 + ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr6);
+        assertEquals(3.0, expr6.eval(MapVarTable.empty()));
+
+        Expression expr7 = RpnParser.parse("1 x y ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr7);
+        assertEquals(1.0, expr7.eval(MapVarTable.of("x",1,"y",2)));
+
+        Expression expr8 = RpnParser.parse("1 2 3 4 ?: 5 6 7 ?: ?:", UnaryFunction.mathDefs());
+        assertInstanceOf(Conditional.class, expr8);
+        assertEquals(3.0, expr8.eval(MapVarTable.empty()));
 
     }
 
